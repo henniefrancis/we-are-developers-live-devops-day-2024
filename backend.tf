9aed25ebff170a3583276e4b06d505375002f228
Hennie Francis <hennie@dotcoresolutions.io>
2024-10-02 11:08:15 +0200

    AWS Resources working

--- backend.tf
+++ backend.tf
@@ -11,26 +11,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.69.0"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.3.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "6.5.0"
-    }
   }
 }
 
 provider "aws" {
   region = var.aws_region.af
 }
-
-# provider "azurerm" {
-#   features {}
-# }
-
-# provider "google" {
-#   project = var.google_project.wearedevelopers
-#   region  = var.google_region.us
-# }
