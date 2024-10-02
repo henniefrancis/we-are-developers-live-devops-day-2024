28321112c584da674b3aee72d9dd5585fcfa5aa7
Hennie Francis <hennie@dotcoresolutions.io>
2024-10-02 12:43:03 +0200

    delete

--- aws.tf
+++ aws.tf
@@ -5,13 +5,13 @@
 # See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 ##########################################################################################################################
 
-resource "aws_s3_bucket" "wearedevelopers-aws-s3-bucket" {
-  provider = aws
-  bucket   = "wearedevelopers-s3-bucket"
-  tags = merge(var.default_tags,
-    {
-      cloudProvider = "AWS"
-      event         = "WeAreDevelopers DevOps Day 2024"
-      location      = "Virtual"
-  })
-}
+# resource "aws_s3_bucket" "wearedevelopers-aws-s3-bucket" {
+#   provider = aws
+#   bucket   = "wearedevelopers-s3-bucket"
+#   tags = merge(var.default_tags,
+#     {
+#       cloudProvider = "AWS"
+#       event         = "WeAreDevelopers DevOps Day 2024"
+#       location      = "Virtual"
+#   })
+# }
